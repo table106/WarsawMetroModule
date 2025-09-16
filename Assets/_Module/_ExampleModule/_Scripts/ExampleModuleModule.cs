b684560ab71e45602eb306a974c68490a1092f41
table106 <cytrynek106@gmail.com>
2025-09-16 19:24:52 +0200

    Tutorial and begin add images

---
+++ Assets/_Module/_ExampleModule/_Scripts/ExampleModuleModule.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine; // Monobehaviour
+
+// This is a very simple module with a solve button and a strike button.
+// Additionally, it displays its display name and module id, which can be helpful for testing other modules which depend on other modules being present on the bomb!
+[RequireComponent(typeof(KMBombModule), typeof(KMSelectable))]
+public partial class ExampleModuleModule : MonoBehaviour
+{
+    // This module uses the custom Button component included with the template.
+    [SerializeField] private Button _solveButton;
+    [SerializeField] private Button _strikeButton;
+
+    [SerializeField] private TextMesh[] _displayNameText;
+    [SerializeField] private TextMesh[] _moduleIdText;
+
+    private KMBombModule _module;
+
+    private static int s_moduleCount;
+    private int _moduleId;
+
+#pragma warning disable IDE0051
+    private void Awake() {
+        _moduleId = s_moduleCount++;
+        _module = GetComponent<KMBombModule>();
+
+        // Assign interaction handlers.
+        _solveButton.OnInteract += Solve;
+        _strikeButton.OnInteract += () => { Strike("You pressed the strike button!"); };
+
+        for (int i = 0; i < 2; i++) {
+            _displayNameText[i].text = _module.ModuleDisplayName;
+            _moduleIdText[i].text = _module.ModuleType;
+        }
+    }
+
+    // Delete the rest of the methods since they are not needed.
+#pragma warning restore IDE0051
+
+    public void Log(string message) => Debug.Log($"[{_module.ModuleDisplayName} #{_moduleId}] {message}");
+
+    public void Strike(string message) {
+        Log($"✕ {message}");
+        _module.HandleStrike();
+    }
+
+    public void Solve() {
+        Log("◯ Module solved!");
+        _module.HandlePass();
+
+        // Disable the buttons. This does not disable their animations.
+        _solveButton.IsActive = false;
+        _strikeButton.IsActive = false;
+    }
+}
---
+++ Assets/_Module/_Scripts/Button.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+// * This is a generic animated button component.
+// * Setup instructions: (WIP)
+[RequireComponent(typeof(KMSelectable))]
+public class Button : MonoBehaviour
+{
+    [SerializeField] private ButtonInfo _buttonTypeInfo;
+
+    private KMAudio _audio;
+    private Animator _animator;
+    private KMSelectable _selectable;
+
+    public event Action OnInteract;
+    public event Action OnInteractEnded;
+
+    public bool IsActive { get; set; } = true;
+
+    protected void Awake() {
+        _audio = GetComponentInParent<KMAudio>();
+        _animator = gameObject.AddComponent<Animator>();
+        _selectable = GetComponent<KMSelectable>();
+
+        _animator.runtimeAnimatorController = _buttonTypeInfo.AnimatorController;
+        _selectable.OnInteract += () => {
+            _animator.SetBool("IsPressed", true);
+            _audio.PlaySoundAtTransform(_buttonTypeInfo.ButtonDownSound, transform);
+            if (IsActive)
+                OnInteract?.Invoke();
+            return false;
+        };
+        _selectable.OnInteractEnded += () => {
+            _animator.SetBool("IsPressed", false);
+            _audio.PlaySoundAtTransform(_buttonTypeInfo.ButtonUpSound, transform);
+            if (IsActive)
+                OnInteractEnded?.Invoke();
+        };
+    }
+
+    public void Select() => _selectable.OnInteract();
+    public void Deselect() => _selectable.OnInteractEnded();
+}
