fbe678704d2dd65db2af511378f7b838a73132ce
table106 <cytrynek106@gmail.com>
2025-11-03 21:43:52 +0100

    Missing dot

--- Assets/WarsawMetroModule/Scripts/WarsawMetroModule.cs
+++ Assets/WarsawMetroModule/Scripts/WarsawMetroModule.cs
@@ -252,7 +252,7 @@ public partial class WarsawMetroModule : MonoBehaviour
                 _ambience = _audio.PlaySoundAtTransformWithRef("Ambience", Stage2.transform);
 
                 // announce next
-                Log($"Next station: {_currentStation}");
+                Log($"Next station: {_currentStation}.");
                 yield return StartCoroutine(PlaySound("NastepnaStacja", Stage2.transform, 2.112f));
                 yield return StartCoroutine(PlaySound(NormalizeName(_currentStation), Stage2.transform, 1.776f));
                 _nextStationTime = Random.Range(45, 76);
